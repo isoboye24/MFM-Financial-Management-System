c691483d44dc77797004990b7b9688d40bb5597d
isoboye24 <isobarguy4real@gmail.com>
2026-08-02 21:51:38 +0200

    Expenditure validated

--- MFMFMS.Domain/Entities/Expenditure.cs
+++ MFMFMS.Domain/Entities/Expenditure.cs
@@ -1,4 +1,6 @@
-﻿namespace MFMFMS.Domain.Entities
+﻿using MFMFMS.Domain.Exceptions;
+
+namespace MFMFMS.Domain.Entities
 {
     public class Expenditure
     {
@@ -6,5 +8,51 @@
         public decimal Amount { get; private set; }
         public DateTime Date { get; private set; }
         public string Summary { get; private set; } = string.Empty;
+
+        public Expenditure(decimal amount, DateTime date, string summary)
+        {
+            ValidateAll(amount, date, summary);
+
+            Amount = amount;
+            Date = date;
+            Summary = summary;
+            Id = Guid.CreateVersion7();
+        }
+
+        private Expenditure()
+        {
+            
+        }
+
+        private static void ValidateAll(decimal amount, DateTime date, string summary)
+        {
+            ValidateSummary(summary);
+            ValidateAmount(amount);
+            ValidateDate(date);
+        }
+
+        private static void ValidateSummary(string summary)
+        {
+            if (string.IsNullOrWhiteSpace(summary))
+            {
+                throw new BusinessRuleException("Summary is required.");
+            }
+        }
+
+        private static void ValidateAmount(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new BusinessRuleException("Amount must be greater than zero.");
+            }
+        }
+
+        private static void ValidateDate(DateTime date)
+        {
+            if (date == DateTime.MinValue)
+            {
+                throw new BusinessRuleException("Date is required.");
+            }
+        }
     }
 }
