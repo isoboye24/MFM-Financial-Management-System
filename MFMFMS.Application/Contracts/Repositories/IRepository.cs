2e96fcf581e0a19f8292913178ec83a5d438999d
isoboye24 <isobarguy4real@gmail.com>
2026-08-04 00:08:57 +0200

    IRepository created.

---
+++ MFMFMS.Application/Contracts/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace MFMFMS.Application.Contracts.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetByYear(int year);
+        Task<IEnumerable<T>> GetByMonth(int month);
+        Task<T?> GetById(Guid id);
+        Task<T?> GetBack(Guid id);
+        Task<T> Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task DeletePermanently(T entity);
+        Task<int> GetTotalAmountOfRecords();
+    }
+}
