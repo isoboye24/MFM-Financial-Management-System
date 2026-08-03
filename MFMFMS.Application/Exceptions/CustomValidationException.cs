c3f77145ce2a80ab15fd7a3dedda30c99ce4f9b3
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-08-03 21:01:51 +0200

    Notfound,Exception MediatorException and CustomValidationException created.

---
+++ MFMFMS.Application/Exceptions/CustomValidationException.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+
+namespace MFMFMS.Application.Exceptions
+{
+    public class CustomValidationException : Exception
+    {
+        public List<string> ValidatorErrors { get; set; } = [];
+
+        public CustomValidationException(string errorMessage)
+        {
+            ValidatorErrors.Add(errorMessage);
+        }
+
+        public CustomValidationException(ValidationResult validationResult)
+        {
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidatorErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ MFMFMS.Application/Exceptions/MediatorException.cs
@@ -0,0 +1,10 @@
+﻿namespace MFMFMS.Application.Exceptions
+{
+    public class MediatorException : Exception
+    {
+        public MediatorException(string message)
+            : base(message)
+        {
+        }
+    }
+}
