dd795f30a5299fcd0a98e07f4bb854be355b4963
isoboye24 <isobarguy4real@gmail.com>
2026-08-03 14:28:20 +0200

    Member entity validated.

--- MFMFMS.Domain/Entities/Member.cs
+++ MFMFMS.Domain/Entities/Member.cs
@@ -1,4 +1,6 @@
-﻿namespace MFMFMS.Domain.Entities
+﻿using MFMFMS.Domain.Exceptions;
+
+namespace MFMFMS.Domain.Entities
 {
     public class Member
     {
@@ -9,5 +11,71 @@
         public string PhoneNumber { get; private set; } = string.Empty;
         public Guid PositionId { get; private set; }
         public Position? Position { get; private set; }
+
+        public Member(string firstName, string lastName, string address, string phoneNumber, Guid positionId)
+        {
+            ValidateAll(firstName, lastName, address, phoneNumber, positionId);
+
+            FirstName = firstName.Trim();
+            LastName = lastName.Trim();
+            Address = address.Trim();
+            PhoneNumber = phoneNumber.Trim();
+            PositionId = positionId;
+            Id = Guid.CreateVersion7();
+        }
+
+        private Member()
+        {
+            
+        }
+
+        private static void ValidateAll(string firstName, string lastName, string address, string phoneNumber, Guid positionId)
+        {
+            ValidateFirstName(firstName);
+            ValidateLastName(lastName);
+            ValidateAddress(address);
+            ValidatePhoneNumber(phoneNumber);
+            ValidatePositionId(positionId);
+        }
+
+        private static void ValidateFirstName(string firstName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new BusinessRuleException("First Name is required.");
+            }
+        }
+        
+        private static void ValidateLastName(string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new BusinessRuleException("Last Name is required.");
+            }
+        }
+        
+        private static void ValidateAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                throw new BusinessRuleException("Address is required.");
+            }
+        }
+        
+        private static void ValidatePhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                throw new BusinessRuleException("Phone Number is required.");
+            }
+        }
+
+        private static void ValidatePositionId(Guid positionId)
+        {
+            if (positionId == Guid.Empty)
+            {
+                throw new BusinessRuleException("Position is required.");
+            }
+        }
     }
 }
