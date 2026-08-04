0bf8cab45ab854e43abc9ec5ba6dc40388785694
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-08-04 16:16:35 +0200

    Document test implemented.

---
+++ MFMFMS.Test/Domain/Entities/DocumentTests.cs
@@ -0,0 +1,32 @@
+﻿using MFMFMS.Domain.Entities;
+using MFMFMS.Domain.Enums;
+
+namespace MFMFMS.Test.Domain.Entities
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        [TestMethod]
+        public void Should_Create_Document_When_Valid_Data_Is_Provided()
+        {
+            var name = "Monthly Report";
+            var blobName = "documents/monthly-report.pdf";
+            var documentType = DocumentType.Report;
+
+            var beforeCreation = DateTime.UtcNow;
+
+            var document = new Document(name, blobName, documentType);
+
+            var afterCreation = DateTime.UtcNow;
+
+            Assert.AreNotEqual(Guid.Empty, document.Id);
+            Assert.AreEqual(name, document.Name);
+            Assert.AreEqual(blobName, document.BlobName);
+            Assert.AreEqual(documentType, document.DocumentType);
+
+            Assert.IsTrue(
+                document.UploadedAt >= beforeCreation &&
+                document.UploadedAt <= afterCreation);
+        }
+    }
+}
