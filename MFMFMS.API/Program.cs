601ffd7a1ccefa8a6a7645128b55256ce5d40566
isoboye24 <isobarguy4real@gmail.com>
2026-08-05 00:24:34 +0200

    Persistence automatic dependency injection and Connection strings created.

--- MFMFMS.API/Program.cs
+++ MFMFMS.API/Program.cs
@@ -1,10 +1,23 @@
+using DotNetEnv;
+using MFMFMS.Application;
+using MFMFMS.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.TraversePath().Load();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 var app = builder.Build();
 
--- MFMFMS.Persistence/DependencyInjection.cs
+++ MFMFMS.Persistence/DependencyInjection.cs
@@ -1,6 +1,28 @@
-﻿namespace MFMFMS.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MFMFMS.Persistence
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            // Register the DbContext with the connection string from configuration
+            services.AddDbContext<MFMFMSDBContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("MFMFMSConnection")));
+
+            // Register other persistence-related services here if needed
+            services.Scan(scan => scan
+                .FromAssembliesOf(typeof(DependencyInjection))
+                .AddClasses()
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+
+            return services;
+        }
     }
 }
