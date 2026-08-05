cb32fbaaaa0926b89a2dc40bbf0dd68cf481b192
isoboye24 <isobarguy4real@gmail.com>
2026-08-05 16:05:12 +0200

    Category configured

---
+++ MFMFMS.Persistence/Configurations/CategoryConfig.cs
@@ -0,0 +1,16 @@
+﻿using MFMFMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MFMFMS.Persistence.Configurations
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(prop => prop.Name)
+               .IsRequired()
+               .HasMaxLength(250);
+        }
+    }
+}
