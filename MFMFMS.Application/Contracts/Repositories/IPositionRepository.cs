d1c54252f8629567759715f960feafc0238bd46e
isoboye24 <isobarguy4real@gmail.com>
2026-08-07 23:41:11 +0200

    Position repository interface created.

---
+++ MFMFMS.Application/Contracts/Repositories/IPositionRepository.cs
@@ -0,0 +1,13 @@
+﻿using MFMFMS.Application.Features.Positions.Queries.GetDeletedPositionLists;
+using MFMFMS.Application.Features.Positions.Queries.GetPositionLists;
+using MFMFMS.Domain.Entities;
+
+namespace MFMFMS.Application.Contracts.Repositories
+{
+    public interface IPositionRepository : IRepository<Position>
+    {
+        Task<IEnumerable<Position>> GetFiltered(PositionFilterDTO filter);
+
+        Task<IEnumerable<Position>> GetDeletedFiltered(DeletedPositionsFilterDTO filter);
+    }
+}
