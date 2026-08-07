7530534a58461cddad358ae293ab0e1e324b35fe
isoboye24 <isobarguy4real@gmail.com>
2026-08-07 23:43:54 +0200

    Position configured.

---
+++ MFMFMS.Persistence/Configurations/PositionConfig.cs
@@ -0,0 +1,16 @@
+﻿using MFMFMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MFMFMS.Persistence.Configurations
+{
+    public class PositionConfig : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.Property(prop => prop.Name)
+               .IsRequired()
+               .HasMaxLength(250);
+        }
+    }
+}
